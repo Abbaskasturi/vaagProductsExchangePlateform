512e0026b386372c14f679c1c502e6ba151f7e09
kasturiabbas <kasturiabbaspatel@gmail.com>
2025-08-13 16:25:12 +0530

    fix error

--- models/user.js
+++ models/user.js
@@ -1,13 +1,9 @@
 'use strict';
 const { Model } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // Defines that a User can have many of each item
       User.hasMany(models.Laptop, { foreignKey: 'userId', as: 'laptops' });
@@ -19,25 +15,56 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Notification, { foreignKey: 'userId', as: 'notifications' });
       User.hasMany(models.Rental, { foreignKey: 'renterId', as: 'rentals' });
     }
+
+    // Method to securely compare passwords during login
+    async validPassword(password) {
+      return await bcrypt.compare(password, this.password);
+    }
   }
 
   User.init({
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Name field cannot be empty.'
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'Please enter a valid email address.'
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        len: {
+          args: [8, 100],
+          msg: 'Password must be at least 8 characters long.'
+        }
+      }
     }
   }, {
     sequelize,
     modelName: 'User',
+    tableName: 'users', // <-- IMPORTANT: Fixes the 'Users doesn't exist' error
+    hooks: {
+      // Hook to automatically hash the password before a user is created
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    }
   });
   
   return User;
